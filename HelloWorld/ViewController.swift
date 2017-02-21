f733093c4dcb153ee96689bbdf3a340d2e854270
Rickard Wahlander <rickardwahlander@rickards-macbook-air.local>
2017-02-21 21:03:21 +0100

    HelloWorld app

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showMessage() {
+        let alertController = UIAlertController(title: "Välkommen till min första app!", message:"Hej Sverige", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler: nil))
+                    present(alertController, animated: true, completion: nil)
+        }
 
 }
 
